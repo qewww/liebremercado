591cc23ccc36d1049b3bd82bfabb086d8ba82684
Heidi Dubois <duboisheidi.hd@gmail.com>
2021-11-18 20:42:01 -0300

    cambio

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
 const express = require('express');
+
 const app = express();
-app.use(express.static(__dirname + '/public'));
 
+app.use(express.static(__dirname + /'public'));
 
-app.listen(process.env.PORT || 3000, ()=>{
+app.listen(3000, ()=>{
     console.log('Servidor funcionando');
 });
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
-});
-gi
-app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
-});
-
-app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
 });
