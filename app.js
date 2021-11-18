8563ad8feb217031370a1249536fe43b899f4154
Heidi Dubois <duboisheidi.hd@gmail.com>
2021-11-18 20:31:13 -0300

    agregando dos archivos

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 
 app.listen(process.env.PORT || 3000, ()=>{
@@ -10,7 +10,7 @@ app.listen(process.env.PORT || 3000, ()=>{
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
 });
-
+gi
 app.get('/login', (req,res)=>{
     res.sendFile(__dirname + '/views/login.html');
 });
