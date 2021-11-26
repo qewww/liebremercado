630f0add0f8545e94f1fe8e169a09f26da9bb83a
Heidi Dubois <duboisheidi.hd@gmail.com>
2021-11-26 13:31:08 -0300

    agregando instalaciones

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const app = express();
+app.use(express.static('public'));
 
-const path = require("path");
-app.use(express.static("public"));
 
 app.listen(3000, ()=>{
     console.log('Servidor funcionando');
 });
 
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/views/home.html');
 });
 
 app.get('/login', (req,res)=>{
